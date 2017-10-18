03ddb80ef04c8917779b90885f8fd396eba873f9
areegechaudhary <areegechaudhary@gmail.com>
2017-10-18 01:12:16 -0400

    Test commit

--- a2.py
+++ a2.py
@@ -1,5 +1,12 @@
 def main():
-	login()
+	sessionType = login()
+
+	transaction = input("Please enter a transaction code: ")
+	if (transaction == "createacct"):
+		if (sessionType == "machine"):
+			print("Sorry, this transaction is unauthorized.")
+		else:
+			createacct()
 	
 
 def login():
@@ -9,20 +16,26 @@ def login():
 	type = input("What type of session would you like?")
 	while ((type != "machine") and (type != "agent")): #do not accept any other input except 'machine' or 'agent'
 		type = input("What type of session would you like?")
-	#read accounts file
+	return type
 
 #def readAccounts(file):
 
-def createacct(type):
+def createacct():
 	if (type == "machine"):
 		print("Sorry, this transaction is unauthorized.")
 	else:
 		number = input("Enter a new account number: ")
-		#check to see if number is 7 digits, doesnt begin with zero and isn't already in the account file
+		if ((str(number[0]) == "0") or (str(number[(len(number)-1)]) == "0")): 
+			print("The account number cannot start or end with 0")
+		if (len(str(number)) != 7): 
+			print("Make sure that the account number is composed of 7 digits")
+		#check to see if number is already in the account file (it shouldn't be)
 		name = input("Enter a name for the account: ")
+		if (name)
 		#check that length of account number is between 3 and 30 alphanumeric chars and that it doesnt begin or end with a space
 		#add number and name info to transaction summary file
 		#don't allow any transaction on new account
+		transaction = input("Please enter a transaction code: ")
 
 def deleteacct(type):
 	if (type == "machine"):
